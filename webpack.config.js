1326b6c6086a9685b05b77236bfc791316674928
Ivan Hui <vanhui@ucdavis.edu>
2018-09-23 18:07:39 -0700

    grunted

--- webpack.config.js
+++ webpack.config.js
@@ -5,9 +5,9 @@ const DIST_DIR = path.join(__dirname, '/client/dist');
 
 
 module.exports = {
-  entry: `${SRC_DIR}/index.jsx`,
+  entry: `${SRC_DIR}/render.jsx`,
   output: {
-    filename: 'bundle.js',
+    filename: 'carousel.js',
     path: DIST_DIR,
   },
   module: {
